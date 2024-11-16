833faf37b7421dfb1a2b0028d87b82f23311322e
Vinicius Martins Valim Gonçalves <viniciusmvg@hotmail.com>
2024-11-16 20:10:06 -0300

    Atualizando o código com as últimas modificações

---
+++ scripts/professores.js
@@ -0,0 +1,203 @@
+const professores = {
+    adison: {
+        nome: "Adilson Nunes Ferreira de Souza",
+        foto: "../icons/adison.jpg",
+        biografia: "Possui Pós-Graduação e Especialização em Docência para o Ensino Superior pela UNIP,  Licenciatura em Matemática pela UNIVESP, Complementação Pedagógica em Matemática pela UNICSUL e  Bacharelado em Ciência da Computação pela USJT. Ao longo de 15 anos, acumulou experiência na área administrativa e financeira, bem como analista de sistemas em empresas do setor privado na área de Logística. Atualmente, exerce a função de Docente no Colégio Jean-Jacques Rousseau, onde está engajado desde 2014, e na UNIFECAF, onde iniciou sua trajetória em fevereiro de 2023.",
+        temas: "Matemática Aplicada, Aprendizado de Máquina, Visão Computacional",
+        contato: "Email: adilson.ferreira@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/adilsonnunesfsouza/",
+        lattes: "https://lattes.cnpq.br"
+    },
+    ailton: {
+        nome: "Ailton Quadrante Freitas",
+        foto: "../icons/ailton.jpg",
+        biografia: "Engenheiro eletrônico formado pela FEI, com MBA em Gestão de Projetos no Instituto Mauá de Tecnologia, pós-graduação em administração Industrial pela Vanzolini (Escola Politécnica USP) e em Análise de Sistemas pela FASP. Tenho atuado nos últimos anos como gerente de produtos, sendo responsável pelas áreas de desenvolvimento de software, hardware, firmware e mecânica. Experiência em atividade técnica comercial, fazendo levantamento de necessidades em clientes para desenvolvimento de projetos especiais, ou mesmo para a inovação dos produtos já existentes. Atuou como professor há mais de 30 anos ministrando aulas em cursos técnicos, graduação e cursos fechados para empresas.",
+        temas: "IOT – Internet das Coisas, desenvolvimento de hardware e desenvolvimento de software.",
+        contato: "Email: ailton.freitas@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/ailton-quadrante-freitas-a2a6039",
+        lattes: "https://lattes.cnpq.br/4780500462301594"
+    },
+    alan: {
+        nome: "Alan Rodrigo Navia",
+        foto: "../icons/alan.jpg",
+        biografia: "Possui Mestrado em Engenharia Elétrica pela POLI-USP (2003), Graduação em Microeletrônica pela FATEC-SP (2001) e Especialização em Ciência de Dados pela PUC-MG (em andamento). Atualmente é Consultor de BI da GLOBAL HITSS e Docente dos cursos de Engenharia e Tecnologia da FECAF. Tem experiência acadêmica e corporativa nas seguintes áreas: Ciência de Dados, Bancos de Dados, Programação de Computadores, Estatística, BI e Indicadores.",
+        temas: "Banco de Dados, Análise de Dados, BI e Ciência de Dados.",
+        contato: "Email: alan.navia@pro.fecaf.com.br",
+        linkedin: "https://br.linkedin.com/in/alan-rodrigo-navia-7a687955",
+        lattes: "http://lattes.cnpq.br/4333469350502528"
+    },
+    ancelmo: {
+        nome: "ancelmo",
+        foto: "../icons/ancelmo.jpg",
+        biografia: "Formação técnica em segurança do trabalho, tecnologia em gestão ambiental e graduado em engenharia ambiental na Universidade São Marcos, pós-graduado em engenharia de segurança do trabalho pela POLI-USP. Licenciatura em matemática, pós-graduação em docência no ensino superior pela Cruzeiro do Sul, mestre em novas tecnologias na educação pelo MUST University. Possuo 18 anos experiência como consultor em segurança,  medicina do trabalho e meio ambiente no seguimento da indústria, logística e construção civil. Experiência como perito em ações trabalhistas e licenciamento ambiental. ",
+        temas: "Meio Ambiente, Segurança e medicina ocupacional e Prevenção de combate a incêndio.",
+        contato: "Email: ancelmo.nascimento@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/ancelmo-nascimento-3b39511b1/",
+        lattes: "http://lattes.cnpq.br/9440145439693939"
+    },
+    anderson: {
+        nome: "anderson",
+        foto: "../icons/anderson.jpg",
+        biografia: "Engenheiro Civil (UniAnchieta); Tecnólogo em Edificações, Obras de Terra e Pavimentação (FATEC-SP); Pós Graduado em Eng. de Pavimentação (FAVENI); Pós Graduado em Estruturas em Concreto Armado e Fundações (UNIPAR); Pós Graduado em Docência (Barão de Mauá). Possui larga experiência em Obras Residenciais, Industriais, Pavimentação.  Elabora Laudos, Pareceres Técnicos e de Segurança Estrutural, bem como atua em processos judiciais como Perito Judicial ou Assistente Técnico. Elabora Análise de Editais de Obras Civis, Propostas Técnicas e Orçamentos para Concorrências Públicas e Privadas.",
+        temas: "Perícia Técnica (Patologias na Construção),  Estruturas em Concreto/Fundações, Eng. de Tráfego e Pavimentação, Orçamentação e Concorrência de Obras..",
+        contato: "Email: anderson.novaes@pro.fecaf.com.br",
+        lattes: "http://lattes.cnpq.br/3931085963468427"
+    },
+    andre: {
+        nome: "André Buono Silveira",
+        foto: "../icons/andre.jpg",
+        biografia: "Professor na FECAF desde 2015. Engenheiro Civil pela Universidade Presbiteriana Mackenzie, MBA em Gestão Empresarial pela FGV, Mestre em Administração de Empresas pela Florida Christian University, Mestre em Gestão de Projetos de Engenharia pela Edinburgh Napier University e Doutorando em Estudos Globais pela Universidade Aberta de Portugal.",
+        temas: "Gestão de Projetos, Gerenciamento e planejamento de Empreendimentos, BIM - Building Information Modeling, Facilities Management, Lean Construction.",
+        contato: "Email: andre.silveira@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/andre-buono-silveira-6b24726/",
+        lattes: "https://lattes.cnpq.br/6371162832932243"
+    },
+    andreluis: {
+        nome: "Andre Luis Souza Santiago",
+        foto: "../icons/andre_santiago.jpg",
+        biografia: "Engenheiro Civil (UAM), Mestre em Habitação: Planejamento e Tecnologia em Construção de Edifícios (IPT - 2023). Facility Manager e Engenheiro com mais de 10 anos de experiência em empresas nacionais, no segmento de Tecnologia, Construção Civil, Cultura e Educação, com foco na Engenharia de Sistemas Prediais, englobando Pesquisas, Planejamento, Infraestrutura Predial, Gestão de Projetos, Retrofit de Sistemas Prediais, Obras hidráulicas, com projetos implantados. Know-how na elaboração de planejamento estratégico, com linhas de pesquisas ligadas à Engenharia de Sistemas Prediais.",
+        temas: "Captação, tratamento e distribuição de água, fontes renováveis de energia, Engenharia Hidráulica, Sistemas Prediais, Facilities Management.",
+        contato: "Email: andre.santiago@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/andresantiagoo/",
+        lattes: "http://buscatextual.cnpq.br/buscatextual/visualizacv.do;jsessionid=0845A83F56CAAF1251B0D960063A648D.buscatextual_0"
+    },    
+    simone: {
+        nome: "Simone Aparecida Alves Pinto",
+        foto: "../icons/simone.jpg",
+        biografia: "Tecnóloga em Elétrica e Eletrônica pela Universidade Mackenzie, Licenciada em Elétrica e Matemática, Graduada em Ciências Contábeis. Especialização em Docência do Ensino Superior, em Automação Industrial, em Sistemas de Energia Fotovoltaica, e em MBA Executivo de Administração Financeira.",
+        temas: "Sistemas Embarcados, Automação Residencial e Industrial, Projetos de Energia Renovável, Projetos de Elétrica e Eletrônica.",
+        contato: "Email: simone.aparecida@pro.fecaf.com.br",
+        linkedin: "https://linkedin.com/in/simone-alves-6a9927247",
+        lattes: "http://lattes.cnpq.br/6850538937095381"
+    },
+    
+    carlos: {
+        nome: "Carlos Eduardo Fontoura",
+        foto: "../icons/carlos_fontoura.jpg",
+        biografia: "Professor na Unifecaf desde 2022. Bacharel em Física Teórica pela Universidade Federal do Rio Grande do Sul (FURG) (2005); Mestre em Física Teórica e Computacional pela Universidade Estadual Paulista Júlio de Mesquita Filho (Unesp) (2007); Doutor em Física Teórica e Computacional pela Universidade Estadual Paulista Júlio de Mesquita Filho (Unesp) (2011); Pós-Doutor pela Universidad de Salamanca (USUAL, Espanha) (2014); Jovem Pesquisador na Universidade Estadual Paulista (IFT/Unesp, SP) (2015); Pós-doutor pelo Instituto Tecnológico de Aeronáutica (ITA, SP) (2019).",
+        temas: "Modelagem Numérica e Computacional, Redes Neurais Artificiais Aplicadas em Eficiência Energética, Métodos Quantitativos para Análise de Dados, Computação Quântica e Neuromórfica.",
+        contato: "Email: carlos.fontoura@pro.fecaf.com.br",
+        lattes: "http://lattes.cnpq.br/9669634447149598"
+    },
+    daniel: {
+        nome: "Daniel Castro Santos",
+        foto: "../icons/daniel_castro.jpg",
+        biografia: "Engenheiro Civil pela UFAM. Mestrado em Habitação, com ênfase em tecnologia na construção de edifícios pelo IPT/SP. MBA em Gerenciamento de Projetos pela FGV. Especialização em Gestão de Negócios Imobiliários pela Fundação Dom Cabral (FDC).",
+        temas: "Gestão de Obras, Construção Civil, Planejamento de Obras, Estradas, Gestão da Inovação, Gestão de Projetos.",
+        contato: "Email: daniel.castro@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/daniel-de-castro-santos-27b438157",
+        lattes: "http://lattes.cnpq.br/9427613922230774"
+    },
+    
+    fabio: {
+        nome: "Fábio Alves Larsen",
+        foto: "../icons/fabio_larsen.jpg",
+        biografia: "Engenheiro de Automação e Sistemas formado pela Universidade Paulista (UNIP). Pós-graduado em Gestão da Qualidade. Professor da UniFECAF desde 2017 nos cursos de Engenharia, nas modalidades presencial e EAD. Possui ampla experiência na indústria de eletroeletrônicos, equipamentos médicos e indústria de transformação.",
+        temas: "Sistemas Embarcados, Automação, Geração de Energia, Eletrotécnica Geral e Eletroeletrônica.",
+        contato: "Email: fabio.larsen@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/fabio-larsen-725432179/",
+        lattes: "https://lattes.cnpq.br/0496411965954216"
+    },
+    
+    francisco: {
+        nome: "Francisco Jose Dias Junior",
+        foto: "../icons/francisco_junior.jpg",
+        biografia: "Tecnólogo em Eletrônica, Bacharel em Administração, Mestre em Engenharia de Produção e Doutorando em Engenharia de Produção. Atuou como Gerente de Engenharia na Sharp do Brasil, Gerente da Qualidade na TCE e Gerente de Serviços Técnicos na Simpress (empresa do grupo HP). Professor da UniFECAF desde 2015, ministrando disciplinas nas áreas de gestão e engenharias.",
+        temas: "Gestão da Qualidade e Produtividade, Pesquisa Operacional, Simulação em Sistemas Discretos, Gestão de Serviços, Gestão da Produção, Projeto de Fábricas.",
+        contato: "Email: francisco.junior@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/francisco-jos%C3%A9-dias-junior-4020a1133/",
+        lattes: "http://lattes.cnpq.br/2494994641385642"
+    },
+    
+    isabella: {
+        nome: "Isabella Narciso",
+        foto: "../icons/isabella.jpg",
+        biografia: "Arquiteta e Urbanista; Mestre em Arquitetura e Urbanismo (FAUMACK). É fundadora do escritório NARQ - Narciso Arquitetura, que atua com desenvolvimento de projetos e acompanhamento de obras comerciais e residenciais. Suas pesquisas são focadas na urbanização da cidade de São Paulo, com ênfase em estudos de espaços públicos e apropriação de espaços livres privados de uso coletivo.",
+        temas: "Projetos de Arquitetura (comercial, institucional, cultural), Urbanismo, Espaços Públicos.",
+        contato: "Email: isabella.narciso@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/isabellagadotti/",
+        lattes: "http://lattes.cnpq.br/9128006441688126"
+    },
+    
+    luiz: {
+        nome: "Luiz Antônio do Nascimento",
+        foto: "../icons/luiz.jpg",
+        biografia: "Professor da UniFecaf desde 2019. Mestrado em Engenharia Civil pela Escola Politécnica da USP, Licenciatura em Matemática pela Uniban, Tecnólogo em Edifícios pela FATEC/SP, Especialização em Gestão Ambiental pela Anhanguera e Especialização em Análise e Projeto de Sistemas pela UNIP. Pesquisa focada no uso da Tecnologia da Informação na Construção Civil.",
+        temas: "Obras de Terra, Fundações, Tecnologia da Informação na Construção Civil.",
+        contato: "Email: luiz.nascimento@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/luiz-antonio-do-nascimento-5ab53519/",
+        lattes: "http://lattes.cnpq.br/0073286701265368"
+    },
+    
+    odair: {
+        nome: "Odair Fernandes de Souza",
+        foto: "../icons/odair.jpg",
+        biografia: "Engenheiro Eletricista e Licenciado em Matemática, com especialização em Educação Matemática. Apaixonado pela Elétrica e Matemática, atua há mais de 13 anos na indústria de projetos elétricos e eletrônicos, voltados para prototipação de eletrodomésticos, eletroeletrônicos, ferramentas e sistemas de refrigeração. Professor na UniFecaf, lecionando disciplinas de Cálculo e diversas disciplinas específicas da Engenharia Elétrica.",
+        temas: "Sistemas Embarcados, Prototipação de Projetos Elétricos e Eletrônicos, Energias Renováveis, Projetos Elétricos Residenciais, Gestão de Custos e Viabilidade.",
+        contato: "Email: odair.souza@pro.fecaf.com.br",
+        linkedin: "https://www.linkedin.com/in/odair-fernandes-5328b61b2",
+        lattes: "http://lattes.cnpq.br/6083268541898783"
+    },
+    
+    pedro_muniz: {
+        nome: "Pedro Guimarães Muniz",
+        foto: "../icons/pedro.jpg",
+        biografia: "Engenheiro Civil, Mestre em Tecnologia da Construção pelo IPT - Instituto de Pesquisas Tecnológicas do Estado de São Paulo. Possui especialização na área de Engenharia de Transportes, com ênfase no modal portuário. Atua como consultor na área de diagnóstico, recuperação e reforço de estruturas de concreto.",
+        temas: "Tecnologia do Concreto, Recuperação e Reforço de Estruturas, Inspeção e Diagnóstico de Estruturas.",
+        contato: "Email: pedro.muniz@pro.fecaf.com.br",
+        linkedin: "https://br.linkedin.com/in/pedro-guimaraes-muniz-042852174",
+        lattes: "http://lattes.cnpq.br/3146005319806376"
+    },
+    
+    rafaela: {
+        nome: "Rafaela da Silva Pinto",
+        foto: "../icons/rafaela.jpg",
+        biografia: "Engenheira de Produção e Qualidade pelo Centro Universitário Educacional de Guaxupé (UNIFEG). Pós-graduada em Gestão de Logística pela UNIFEG e em Gestão de Projetos pela USP/ESALQ. Mestranda em Tecnologias Emergentes da Educação pela Must University. Professora na UNIFECAF desde agosto de 2022.",
+        temas: "Gestão da Qualidade, Gestão de Projetos, Gestão de Logística, Gestão de Serviços, Projeto e Desenvolvimento de Produto.",
+        contato: "Email: rafaela.silva@pro.fecaf.com.br",
+        linkedin: "https://linkedin.com/in/rafaela-pinto-23",
+        lattes: "http://lattes.cnpq.br/0725972148211087"
+    },
+    
+    roberto: {
+        nome: "Roberto Eduardo Bruzetti Leminsk",
+        foto: "../icons/roberto.jpg",
+        biografia: "Tecnólogo em Materiais, Processos e Componentes Eletrônicos (MPCE) pela FATEC-SP (1998), com mestrado em Engenharia Elétrica (ênfase em Microeletrônica) pela EPUSP (2001). Exerce a docência na área da Computação e Matemática desde 2002. Autor de material didático e instrucional para IES. Colaborador do Instituto Brasileiro de Formação e Capacitação (IBFC) para a elaboração e aplicação de concursos públicos. É docente dos cursos de Engenharia da UNIFECAF desde fevereiro de 2023.",
+        temas: "Algoritmos e Pensamento Computacional, Sistemas Computacionais, Análise de Dados e Inteligência Artificial.",
+        contato: "Email: roberto.leminski@pro.fecaf.com.br",
+        linkedin: "",
+        lattes: "http://lattes.cnpq.br/1506184471226537"
+    }    
+    // Adicione outros professores aqui
+};
+
+
+// Função para obter o valor de um parâmetro da URL
+function getURLParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+// Quando a página carregar
+window.onload = function() {
+    // Obtém o id do professor da URL (por exemplo, ?id=adison)
+    const professorId = getURLParameter('id');
+
+    // Verifica se o id existe e se há dados para esse professor
+    if (professorId && professores[professorId]) {
+        const professor = professores[professorId];
+
+        // Preenche os elementos HTML com as informações do professor
+        document.getElementById('foto-professor').src = professor.foto;
+        document.getElementById('foto-professor').alt = `Foto de ${professor.nome}`;
+        document.getElementById('nome-professor').textContent = professor.nome;
+        document.getElementById('biografia').textContent = professor.biografia;
+        document.getElementById('temas').textContent = professor.temas;
+        document.getElementById('contato').textContent = professor.contato;
+        document.getElementById('linkedin').href = professor.linkedin;
+        document.getElementById('lattes').href = professor.lattes;
+    } else {
+        // Caso o id não seja válido ou não exista, exibe uma mensagem de erro
+        document.body.innerHTML = '<h1>Professor não encontrado!</h1><p>O professor solicitado não existe. Verifique o ID e tente novamente.</p>';
+    }
+};
